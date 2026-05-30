4781c077991976c12323469b7ab336417cf251ad
Nguyễn Vương Văn Khiêm <nguyenvuongvankhiem1132@gmail.com>
2026-05-30 13:39:31 +0700

    Time: 1 ms (99.94%), Space: 38.1 MB (51.23%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+                if(x<0) return false;
+        var s = x.ToString();
+        for(int i=0; i < s.Length / 2; i++)
+        {
+            if(s[i] != s[s.Length - 1 - i]) return false;
+        }
+        return true;
+    }
+}
